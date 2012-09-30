4e30046f88d01f4dcd905cb6fa431722c7120bc8
albertoruibal <alberto@alonsoruibal.com>
2012-09-30 14:16:45 +0200

    WebTv GWT interface separated in his own project

---
+++ webtvchess/src/main/java/org/vectomatic/svg/chess/MoveListener.java
@@ -0,0 +1,7 @@
+package org.vectomatic.svg.chess;
+
+public interface MoveListener {
+
+	public void doMove(int move);
+
+}
---
+++ webtvchess/src/main/java/org/vectomatic/svg/chess/Resources.java
@@ -0,0 +1,30 @@
+/**********************************************
+ * Copyright (C) 2009 Lukas Laag
+ * This file is part of lib-gwt-svg-chess.
+ * 
+ * libgwtsvg-chess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libgwtsvg-chess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libgwtsvg-chess.  If not, see http://www.gnu.org/licenses/
+ **********************************************/
+package org.vectomatic.svg.chess;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+public interface Resources extends ClientBundle {
+	public static final Resources INSTANCE =  GWT.create(Resources.class);
+	@Source("chess.svg")
+	public TextResource getBoard();
+	@Source("chess.css")
+	public ChessCss getCss();
+}
