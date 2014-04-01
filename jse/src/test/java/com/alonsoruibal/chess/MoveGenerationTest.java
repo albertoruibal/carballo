16b45715595be16d9496ef3d375defe6e8f2fd9f
albertoruibal <alberto@alonsoruibal.com>
2014-04-01 10:01:19 +0200

    Improved move generation tests with more cases and results verification

--- jse/src/test/java/com/alonsoruibal/chess/MoveGenerationTest.java
+++ jse/src/test/java/com/alonsoruibal/chess/MoveGenerationTest.java
@@ -1,24 +1,15 @@
 package com.alonsoruibal.chess;
 
-import junit.framework.TestCase;
-
 import com.alonsoruibal.chess.movegen.LegalMoveGenerator;
 import com.alonsoruibal.chess.movegen.MoveGenerator;
 
+import junit.framework.TestCase;
+
 /**
- *
- * 
- * 
- * 
- * 
- * @author rui
+ * Do some move generation tests from http://chessprogramming.wikispaces.com/Perft+Results
+ * Takes a looooong time
  */
 public class MoveGenerationTest extends TestCase {
-	/**
-	 * Logger for this class
-	 */
-//	private static final Logger logger = Logger.getLogger(MoveGenerationTest.class);
-
 	private static final int DEPTH = 7;
 
 	MoveGenerator legalMovegen = new LegalMoveGenerator();
@@ -50,41 +41,91 @@ public class MoveGenerationTest extends TestCase {
 		}
 	}
 
-	public void test1() {
+	public void testInitialPosition() {
 		reset();
-		System.out.println("TEST 1");
+		System.out.println("TEST INITIAL POSITION");
 		board.startPosition();
 		recursive(0, 6);
 		print(6);
+		assertEquals(moveCount[5], 119060324);
+		assertEquals(captures[5], 2812008);
+		assertEquals(passantCaptures[5], 5248);
+		assertEquals(castles[5], 0);
+		assertEquals(promotions[5], 0);
+		assertEquals(checks[5], 809099);
+		assertEquals(checkMates[5], 10828);
 	}
 
-	public void test2() {
+	public void testPosition2() {
 		reset();
-		System.out.println("TEST 2");
+		System.out.println("TEST POSITION 2");
 		board.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
 		recursive(0, 5);
 		print(5);
+		assertEquals(moveCount[4], 193690690);
+		assertEquals(captures[4], 35043416);
+		assertEquals(passantCaptures[4], 73365);
+		assertEquals(castles[4], 4993637);
+		assertEquals(promotions[4], 8392);
+		assertEquals(checks[4], 3309887);
+		assertEquals(checkMates[4], 30171);
 	}
 
-	public void test3() {
+	public void testPosition3() {
 		reset();
-		System.out.println("TEST 3");
+		System.out.println("TEST POSITION 3");
 		board.setFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
 		recursive(0, 7);
 		print(7);
+
+		assertEquals(moveCount[6], 178633661);
+		assertEquals(captures[6], 14519036);
+		assertEquals(passantCaptures[6], 294874);
+		assertEquals(castles[6], 0);
+		assertEquals(promotions[6], 140024);
+		assertEquals(checks[6], 12797406);
+		assertEquals(checkMates[6], 87);
 	}
-	
+
+	public void testPosition4() {
+		reset();
+		System.out.println("TEST POSITION 4");
+		board.setFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+		recursive(0, 6);
+		print(6);
+
+		assertEquals(moveCount[5], 706045033);
+		assertEquals(captures[5], 210369132);
+		assertEquals(passantCaptures[5], 212);
+		assertEquals(castles[5], 10882006);
+		assertEquals(promotions[5], 81102984);
+		assertEquals(checks[5], 26973664);
+		assertEquals(checkMates[5], 81076);
+	}
+
+	public void testPosition5() {
+		reset();
+		System.out.println("TEST POSITION 5");
+		board.setFen("rnbqkb1r/pp1p1ppp/2p5/4P3/2B5/8/PPP1NnPP/RNBQK2R w KQkq - 0 6");
+		recursive(0, 3);
+		print(3);
+
+		assertEquals(moveCount[0], 42);
+		assertEquals(moveCount[1], 1352);
+		assertEquals(moveCount[2], 53392);
+	}
+
 	private void recursive(int depth, int depthRemaining) {
 		int moves[] = new int[256];
-//		logger.debug("\n"+board);
 		int index = legalMovegen.generateMoves(board, moves, 0);
-//		Move.printMoves(moves, index);
 		for (int i = 0; i < index; i++) {
 			int move = moves[i];
-//			logger.debug(depth + "->" + Move.toStringExt(move));
 			board.doMove(move);
 			if (depthRemaining > 0) {
 				moveCount[depth]++;
+				if ((moveCount[depth] % 100000) == 0) {
+					System.out.println("movecount[" + depth + "]=" + moveCount[depth]);
+				}
 				if (Move.isCapture(move)) captures[depth]++;
 				if (Move.getMoveType(move) == Move.TYPE_PASSANT) passantCaptures[depth]++;
 				if (Move.getMoveType(move) == Move.TYPE_KINGSIDE_CASTLING
@@ -92,7 +133,6 @@ public class MoveGenerationTest extends TestCase {
 				if (Move.isPromotion(move)) promotions[depth]++;
 				if (board.getCheck()) {
 					checks[depth]++;
-//					logger.debug("\n"+board);
 				}
 				if (board.isMate()) {
 					checkMates[depth]++;
--- jse/src/test/java/com/alonsoruibal/chess/MoveIteratorTest.java
+++ jse/src/test/java/com/alonsoruibal/chess/MoveIteratorTest.java
@@ -1,24 +1,16 @@
 package com.alonsoruibal.chess;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import junit.framework.TestCase;
-
-import com.alonsoruibal.chess.log.Logger;
 import com.alonsoruibal.chess.movegen.MagicMoveGenerator;
 import com.alonsoruibal.chess.movegen.MoveGenerator;
 import com.alonsoruibal.chess.movesort.MoveIterator;
 import com.alonsoruibal.chess.movesort.SortInfo;
 
-/**
- * Helps searching for bottlenecks
- * 
- * @author rui
- */
-public class MoveIteratorTest extends TestCase {
-	private static final Logger logger = Logger.getLogger("MoveIteratorTest");
+import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveIteratorTest extends TestCase {
 	private static final int DEPTH = 7;
 
 	Board board = new Board();
@@ -42,35 +34,85 @@ public class MoveIteratorTest extends TestCase {
 
 	private void print(int depth) {
 		for (int i = 0; i < depth; i++) {
-			logger.debug("Moves: " + moveCount[i] + " Captures=" + captures[i]
-					+ " E.P.=" + passantCaptures[i] + " Castles=" + castles[i]
-					+ " Promotions=" + promotions[i] + " Checks=" + checks[i]
-					+ " CheckMates=" + checkMates[i]);
+			System.out.println("Moves: " + moveCount[i] + " Captures="
+					+ captures[i] + " E.P.=" + passantCaptures[i] + " Castles="
+					+ castles[i] + " Promotions=" + promotions[i] + " Checks="
+					+ checks[i] + " CheckMates=" + checkMates[i]);
 		}
 	}
 
-	public void test1() {
+	public void testInitialPosition() {
 		reset();
-		System.out.println("TEST 1");
+		System.out.println("TEST INITIAL POSITION");
 		board.startPosition();
 		recursive(0, 6);
 		print(6);
+		assertEquals(moveCount[5], 119060324);
+		assertEquals(captures[5], 2812008);
+		assertEquals(passantCaptures[5], 5248);
+		assertEquals(castles[5], 0);
+		assertEquals(promotions[5], 0);
+		assertEquals(checks[5], 809099);
+		assertEquals(checkMates[5], 10828);
 	}
 
-	public void test2() {
+	public void testPosition2() {
 		reset();
-		System.out.println("TEST 2");
+		System.out.println("TEST POSITION 2");
 		board.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
 		recursive(0, 5);
 		print(5);
+		assertEquals(moveCount[4], 193690690);
+		assertEquals(captures[4], 35043416);
+		assertEquals(passantCaptures[4], 73365);
+		assertEquals(castles[4], 4993637);
+		assertEquals(promotions[4], 8392);
+		assertEquals(checks[4], 3309887);
+		assertEquals(checkMates[4], 30171);
 	}
 
-	public void test3() {
+	public void testPosition3() {
 		reset();
-		System.out.println("TEST 3");
+		System.out.println("TEST POSITION 3");
 		board.setFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
 		recursive(0, 7);
 		print(7);
+
+		assertEquals(moveCount[6], 178633661);
+		assertEquals(captures[6], 14519036);
+		assertEquals(passantCaptures[6], 294874);
+		assertEquals(castles[6], 0);
+		assertEquals(promotions[6], 140024);
+		assertEquals(checks[6], 12797406);
+		assertEquals(checkMates[6], 87);
+	}
+
+	public void testPosition4() {
+		reset();
+		System.out.println("TEST POSITION 4");
+		board.setFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+		recursive(0, 6);
+		print(6);
+
+		assertEquals(moveCount[5], 706045033);
+		assertEquals(captures[5], 210369132);
+		assertEquals(passantCaptures[5], 212);
+		assertEquals(castles[5], 10882006);
+		assertEquals(promotions[5], 81102984);
+		assertEquals(checks[5], 26973664);
+		assertEquals(checkMates[5], 81076);
+	}
+
+	public void testPosition5() {
+		reset();
+		System.out.println("TEST POSITION 5");
+		board.setFen("rnbqkb1r/pp1p1ppp/2p5/4P3/2B5/8/PPP1NnPP/RNBQK2R w KQkq - 0 6");
+		recursive(0, 3);
+		print(3);
+
+		assertEquals(moveCount[0], 42);
+		assertEquals(moveCount[1], 1352);
+		assertEquals(moveCount[2], 53392);
 	}
 
 	private void recursive(int depth, int depthRemaining) {
@@ -84,23 +126,26 @@ public class MoveIteratorTest extends TestCase {
 		}
 
 		// Move.printMoves(moves, index);
-		MoveIterator moveIterator = new MoveIterator(board, new SortInfo(),	depth);
+		MoveIterator moveIterator = new MoveIterator(board, new SortInfo(), depth);
 		moveIterator.genMoves(0);
 		int move = 0;
 		while ((move = moveIterator.next()) != 0) {
 			if (!moveList.contains(move)) {
-				logger.error("\n" + board);
-				logger.error("Move not found: " + Move.toStringExt(move));
+				System.out.println("\n" + board);
+				System.out.println("Move not found: " + Move.toStringExt(move));
 			} else {
 				moveList.remove((Integer) move);
 			}
-			
+
 			// logger.debug(depth + "->" + Move.toStringExt(move));
 			if (board.doMove(move)) {
 
 
 				if (depthRemaining > 0) {
 					moveCount[depth]++;
+					if ((moveCount[depth] % 100000) == 0) {
+						System.out.println("movecount[" + depth + "]=" + moveCount[depth]);
+					}
 					if (Move.isCapture(move))
 						captures[depth]++;
 					if (Move.getMoveType(move) == Move.TYPE_PASSANT)
@@ -124,13 +169,11 @@ public class MoveIteratorTest extends TestCase {
 			}
 		}
 		if (moveList.size() > 0) {
-			logger.debug("\n" + board);
+			System.out.println("\n" + board);
 			while (moveList.size() > 0) {
-				logger.error("Move not generated: "
-						+ Move.toStringExt(moveList.get(0)));
+				System.out.println("Move not generated: " + Move.toStringExt(moveList.get(0)));
 				moveList.remove(0);
 			}
 		}
 	}
-
 }
