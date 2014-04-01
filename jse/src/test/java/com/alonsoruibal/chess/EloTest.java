4f60be91fd9f924864ead9b9604f0e8a5983118d
albertoruibal <alberto@alonsoruibal.com>
2014-04-01 09:34:34 +0200

    Moved all the elo test to the same test class

---
+++ jse/src/test/java/com/alonsoruibal/chess/EloTest.java
@@ -0,0 +1,33 @@
+package com.alonsoruibal.chess;
+
+public class EloTest extends EpdTest {
+
+	public void testBS2830() {
+		long time = processEpdFile(this.getClass().getResourceAsStream("/bs2830.epd"), 15 * 60000);
+		double timeMinutes = time / 60000;
+		double elo = 2850 - (timeMinutes / 1.5) - (timeMinutes * timeMinutes) / (22 * 22);
+		System.out.println("BS2830 Elo = " + elo);
+	}
+
+	public void testBT2450() {
+		long time = processEpdFile(this.getClass().getResourceAsStream("/bt2450.epd"), 15 * 60000);
+		double timeSeconds = time / 1000;
+		double elo = 2450 - (timeSeconds / 30);
+		System.out.println("BT2450 Elo = " + elo);
+	}
+
+	public void testBT2630() {
+		long time = processEpdFile(this.getClass().getResourceAsStream("/bt2630.epd"), 15 * 60000);
+		double timeSeconds = time / 1000;
+		double elo = 2630 - (timeSeconds / 30);
+		System.out.println("BT2630 Elo = " + elo);
+	}
+
+	public void testLCTII() {
+		long time = processEpdFile(this.getClass().getResourceAsStream("/lctii.epd"), 10 * 60000);
+		double timeSeconds = time / 1000;
+		System.out.println("time in seconds = " + timeSeconds);
+		System.out.println("LCTII ELO = " + (1900 + getLctPoints()));
+	}
+
+}
