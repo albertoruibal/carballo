6064ad82caadb9e93f9d4c6278651ada4015f720
albertoruibal <alberto@alonsoruibal.com>
2015-02-17 10:22:54 +0100

    Tests for the new TT

--- jse/src/test/java/com/alonsoruibal/chess/TranspositionTableTest.java
+++ jse/src/test/java/com/alonsoruibal/chess/TranspositionTableTest.java
@@ -1,7 +1,6 @@
 package com.alonsoruibal.chess;
 
 import com.alonsoruibal.chess.evaluation.Evaluator;
-import com.alonsoruibal.chess.tt.MultiprobeTranspositionTable;
 import com.alonsoruibal.chess.tt.TranspositionTable;
 
 import org.junit.Test;
@@ -14,23 +13,25 @@ public class TranspositionTableTest {
 	public void testTraspositionTable() {
 		Board b = new Board();
 		b.startPosition();
-		TranspositionTable tt = new MultiprobeTranspositionTable(20);
+		TranspositionTable tt = new TranspositionTable(20);
 
 		int nodeType = TranspositionTable.TYPE_EXACT_SCORE;
 		int bestMove = Move.getFromString(b, "e2e4", true);
 		int score = -100;
 		byte depthAnalyzed = 10;
-		tt.set(b, nodeType, bestMove, score, depthAnalyzed, false);
+		short eval = 456;
+		tt.set(b, nodeType, bestMove, score, depthAnalyzed, eval, false);
 		tt.search(b, 0, false);
 		assertEquals(nodeType, tt.getNodeType());
 		assertEquals(bestMove, tt.getBestMove());
 		assertEquals(score, tt.getScore());
 		assertEquals(depthAnalyzed, tt.getDepthAnalyzed());
+		assertEquals(eval, tt.getEval());
 	}
 
 	@Test
 	public void testDistanceToInitialPly() {
-		TranspositionTable tt = new MultiprobeTranspositionTable(20);
+		TranspositionTable tt = new TranspositionTable(20);
 		Board b = new Board();
 		b.setFen("8/7K/8/8/8/8/R7/7k w - - 0 1");
 
@@ -38,7 +39,7 @@ public class TranspositionTableTest {
 		int score = Evaluator.VICTORY - 8;
 		int depthAnalyzed = 4;
 		// Must store SearchEngine.VALUE_IS_MATE - 4
-		tt.save(b, 4, depthAnalyzed, bestMove, score, -Evaluator.VICTORY, +Evaluator.VICTORY, false);
+		tt.save(b, 4, depthAnalyzed, bestMove, score, -Evaluator.VICTORY, +Evaluator.VICTORY, 0, false);
 		tt.search(b, 1, false);
 		assertEquals("It does not fix the mate score in the transposition table", tt.getScore(), Evaluator.VICTORY - 5);
 	}
