506f155e1dd868055b1a7736d64b206401dd3e57
albertoruibal <alberto@alonsoruibal.com>
2015-03-13 08:14:37 +0100

    Use 1" for earch WinAtChess test (it was failing 4, now fails 16)

--- jse/src/test/java/com/alonsoruibal/chess/epdtest/WinAtChessTest.java
+++ jse/src/test/java/com/alonsoruibal/chess/epdtest/WinAtChessTest.java
@@ -1,7 +1,6 @@
 package com.alonsoruibal.chess.epdtest;
 
 import com.alonsoruibal.chess.SlowTest;
-import com.alonsoruibal.chess.log.Logger;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -9,43 +8,10 @@ import org.junit.experimental.categories.Category;
 import static org.junit.Assert.assertTrue;
 
 public class WinAtChessTest extends EpdTest {
-	private static final Logger logger = Logger.getLogger("WinAtChessTest");
-
 	@Test
 	@Category(SlowTest.class)
 	public void testWinAtChess() {
-		processEpdFile(this.getClass().getResourceAsStream("/wacnew.epd"), 5000);
-		int solved1 = getSolved();
-		assertTrue(fails <= 6);
-
-//		search.getConfig().setExtensionsPassedPawn(0);
-//		search.getConfig().setExtensionsPawnPush(2);
-//		processEpdFile(this.getClass().getResourceAsStream("/wacnew.epd"), 5000);
-//		int solved2 = getSolved();
-
-//		search.getConfig().setExtensionsPassedPawn(2);
-//		search.getConfig().setExtensionsPawnPush(0);
-//		processEpdFile(this.getClass().getResourceAsStream("/wacnew.epd"), 1000);
-//		int solved3 = getSolved();
-//
-//		search.getConfig().setExtensionsPassedPawn(0);
-//		search.getConfig().setExtensionsPawnPush(2);
-//		processEpdFile(this.getClass().getResourceAsStream("/wacnew.epd"), 1000);
-//		int solved4 = getSolved();
-//
-//		search.getConfig().setExtensionsPassedPawn(2);
-//		search.getConfig().setExtensionsPawnPush(2);
-//		processEpdFile(this.getClass().getResourceAsStream("/wacnew.epd"), 1000);
-//		int solved5 = getSolved();
-
-//		processEpdFile(this.getClass().getResourceAsStream("/wacnew.epd"), 5000);
-//		int solved6 = getSolved();
-
-		logger.debug("Config1 = " + solved1);
-//		logger.debug("Config2 = " + solved2);
-//		logger.debug("Config3 = " + solved3);
-//		logger.debug("Config4 = " + solved4);
-//		logger.debug("Config5 = " + solved5);
-//		logger.debug("Config6 = " + solved6);
+		processEpdFile(this.getClass().getResourceAsStream("/wacnew.epd"), 1000);
+		assertTrue(fails <= 16);
 	}
 }
