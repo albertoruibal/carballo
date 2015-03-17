56137f7096f0a22bede2f0b48745f676d14dcba2
albertoruibal <alberto@alonsoruibal.com>
2015-03-17 19:53:05 +0100

    Arasan tests to one minute each

--- jse/src/test/java/com/alonsoruibal/chess/epdtest/ArasanTest.java
+++ jse/src/test/java/com/alonsoruibal/chess/epdtest/ArasanTest.java
@@ -1,17 +1,15 @@
 package com.alonsoruibal.chess.epdtest;
 
 import com.alonsoruibal.chess.SlowTest;
-import com.alonsoruibal.chess.log.Logger;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 public class ArasanTest extends EpdTest {
-	private static final Logger logger = Logger.getLogger("WinAtChessTest");
 
 	@Test
 	@Category(SlowTest.class)
 	public void testArasan() {
-		processEpdFile(this.getClass().getResourceAsStream("/arasan.epd"), 5000);
+		processEpdFile(this.getClass().getResourceAsStream("/arasan.epd"), 60000);
 	}
 }
