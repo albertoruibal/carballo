df4698967678984e260ccd96e077943b2b6690fa
albertoruibal <alberto@alonsoruibal.com>
2015-03-24 00:30:28 +0100

    Removed unnecessary comment

--- core/src/main/java/com/alonsoruibal/chess/movegen/MoveGenerator.java
+++ core/src/main/java/com/alonsoruibal/chess/movegen/MoveGenerator.java
@@ -2,10 +2,6 @@ package com.alonsoruibal.chess.movegen;
 
 import com.alonsoruibal.chess.Board;
 
-
-/**
- * @author rui
- */
 public interface MoveGenerator {
 
 	public int generateMoves(Board board, int moves[], int index);
