5d34b3c23b3280c493e5371be7a7c0beea784a6c
albertoruibal <alberto@alonsoruibal.com>
2015-06-16 17:23:17 +0200

    SBD test to 1"

--- jse/src/test/java/com/alonsoruibal/chess/epdtest/SilentButDeadlyTest.java
+++ jse/src/test/java/com/alonsoruibal/chess/epdtest/SilentButDeadlyTest.java
@@ -1,17 +1,15 @@
 package com.alonsoruibal.chess.epdtest;
 
 import com.alonsoruibal.chess.SlowTest;
-import com.alonsoruibal.chess.log.Logger;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 public class SilentButDeadlyTest extends EpdTest {
-	private static final Logger logger = Logger.getLogger("WinAtChessTest");
 
 	@Test
 	@Category(SlowTest.class)
 	public void testSilentButDeadly() {
-		processEpdFile(this.getClass().getResourceAsStream("/silentbutdeadly.epd"), 5000);
+		processEpdFile(this.getClass().getResourceAsStream("/silentbutdeadly.epd"), 1000);
 	}
 }
