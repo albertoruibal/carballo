dedfae7ec9fa3317cf9df0f280cddcdd1101ceed
albertoruibal <alberto@alonsoruibal.com>
2016-04-13 22:12:39 +0200

    Test getLastMoveSee for a non capture TT move. Pass TT Move as String to testPositionCountingMoves.

--- jse/src/test/java/com/alonsoruibal/chess/MoveIteratorTest.java
+++ jse/src/test/java/com/alonsoruibal/chess/MoveIteratorTest.java
@@ -11,11 +11,12 @@ import static org.junit.Assert.assertTrue;
 
 public class MoveIteratorTest {
 
-	private void testPositionCountingMoves(String fen, int generateMoves, int ttMove, int totalMoves, int totalCaptures, int totalEnPassant, int totalChecks) {
+	private void testPositionCountingMoves(String fen, int generateMoves, String ttMoveString, int totalMoves, int totalCaptures, int totalEnPassant, int totalChecks) {
 		Board b = new Board();
 		b.setFen(fen);
 		System.out.println(b.toString());
 		MoveIterator moveIterator = new MoveIterator(b, new AttacksInfo(), new SortInfo(), 0);
+		int ttMove = ttMoveString == null ? Move.NONE : Move.getFromString(b, ttMoveString, true);
 		moveIterator.genMoves(ttMove, generateMoves);
 		int move;
 		int moves = 0;
@@ -196,94 +197,91 @@ public class MoveIteratorTest {
 
 	@Test
 	public void enPassantGivesCheck2PiecesXray() {
-		testPositionCountingMoves("8/8/8/R2Pp2k/8/8/8/7K w - e6 0 1", MoveIterator.GENERATE_ALL, 0, 14, 1, 1, 1);
+		testPositionCountingMoves("8/8/8/R2Pp2k/8/8/8/7K w - e6 0 1", MoveIterator.GENERATE_ALL, null, 14, 1, 1, 1);
 	}
 
 	@Test
 	public void cannotCaptureEnPassantCheck2PiecesXray() {
-		testPositionCountingMoves("8/8/8/r2Pp2K/8/8/8/7k w - e6 0 1", MoveIterator.GENERATE_ALL, 0, 6, 0, 0, 0);
+		testPositionCountingMoves("8/8/8/r2Pp2K/8/8/8/7k w - e6 0 1", MoveIterator.GENERATE_ALL, null, 6, 0, 0, 0);
 	}
 
 	@Test
 	public void promoteCapturing() {
-		testPositionCountingMoves("1n5k/2P5/8/8/8/8/8/7K w - - 0 1", MoveIterator.GENERATE_ALL, Move.NONE, 11, 4, 0, 4);
+		testPositionCountingMoves("1n5k/2P5/8/8/8/8/8/7K w - - 0 1", MoveIterator.GENERATE_ALL, null, 11, 4, 0, 4);
 	}
 
 	@Test
 	public void checkEvasions() {
-		testPositionCountingMoves("4k3/8/8/8/1b1Q4/2b5/1P6/4K3 w - - 0 1", MoveIterator.GENERATE_ALL, Move.NONE, 7, 2, 0, 0);
+		testPositionCountingMoves("4k3/8/8/8/1b1Q4/2b5/1P6/4K3 w - - 0 1", MoveIterator.GENERATE_ALL, null, 7, 2, 0, 0);
 	}
 
 	@Test
 	public void avoidCheckPromoting() {
-		testPositionCountingMoves("K6r/1P6/8/8/8/8/8/7k w - - 0 1", MoveIterator.GENERATE_ALL, Move.NONE, 5, 0, 0, 0);
+		testPositionCountingMoves("K6r/1P6/8/8/8/8/8/7k w - - 0 1", MoveIterator.GENERATE_ALL, null, 5, 0, 0, 0);
 	}
 
 	@Test
 	public void checkEvasionCapturingEnPassant() {
-		testPositionCountingMoves("8/8/8/3k4/1pP5/8/8/5K2 b - c3 0 1", MoveIterator.GENERATE_ALL, Move.NONE, 9, 2, 1, 0);
+		testPositionCountingMoves("8/8/8/3k4/1pP5/8/8/5K2 b - c3 0 1", MoveIterator.GENERATE_ALL, null, 9, 2, 1, 0);
 	}
 
 	@Test
 	public void checkEvasionInterposeCapturingEnPassant() {
-		testPositionCountingMoves("8/8/8/8/1pPk4/8/8/B4K2 b - c3 0 1", MoveIterator.GENERATE_ALL, Move.NONE, 6, 2, 1, 0);
+		testPositionCountingMoves("8/8/8/8/1pPk4/8/8/B4K2 b - c3 0 1", MoveIterator.GENERATE_ALL, null, 6, 2, 1, 0);
 	}
 
 	@Test
 	public void captureCheckingPieceWithKing() {
-		testPositionCountingMoves("rq2r1k1/5Qp1/p4p2/4bNP1/1p2P2P/8/PP4K1/5R1R b - - 1 2", MoveIterator.GENERATE_ALL, Move.NONE, 3, 1, 0, 0);
+		testPositionCountingMoves("rq2r1k1/5Qp1/p4p2/4bNP1/1p2P2P/8/PP4K1/5R1R b - - 1 2", MoveIterator.GENERATE_ALL, null, 3, 1, 0, 0);
 	}
 
 	@Test
 	public void captureCheckingPieceWithKingAndTwoPiecesGivingCheck() {
-		testPositionCountingMoves("k4r2/R5pb/1pQp1n1p/3P4/5p1P/3P2P1/r1q1R2K/8 b - - 1 1", MoveIterator.GENERATE_ALL, Move.NONE, 2, 1, 0, 0);
+		testPositionCountingMoves("k4r2/R5pb/1pQp1n1p/3P4/5p1P/3P2P1/r1q1R2K/8 b - - 1 1", MoveIterator.GENERATE_ALL, null, 2, 1, 0, 0);
 	}
 
 	@Test
 	public void evadeCheckMoveKingCapturing() {
-		testPositionCountingMoves("r5r1/p1q2pBk/1p1R2p1/3pP3/6bQ/2p5/P1P1NPPP/6K1 b - - 1 1", MoveIterator.GENERATE_ALL, Move.NONE, 2, 1, 0, 0);
+		testPositionCountingMoves("r5r1/p1q2pBk/1p1R2p1/3pP3/6bQ/2p5/P1P1NPPP/6K1 b - - 1 1", MoveIterator.GENERATE_ALL, null, 2, 1, 0, 0);
 	}
 
 	@Test
 	public void generatingDuplicatedTTMove() {
-		Board b = new Board();
-		b.setFen("rq2r1k1/5p2/p6p/4b1P1/1p2P2P/5Q2/PP4K1/5R1R w - - 0 2");
-
-		testPositionCountingMoves("rq2r1k1/5p2/p6p/4b1P1/1p2P2P/5Q2/PP4K1/5R1R w - - 0 2", MoveIterator.GENERATE_ALL, Move.getFromString(b, "Qf3xf7+", true), 35, 2, 0, 1);
+		testPositionCountingMoves("rq2r1k1/5p2/p6p/4b1P1/1p2P2P/5Q2/PP4K1/5R1R w - - 0 2", MoveIterator.GENERATE_ALL, "Qf3xf7+", 35, 2, 0, 1);
 	}
 
 	@Test
 	public void testGeneratePromotionsInQuiescence() {
 		// It must only generate the promotion to queen
-		testPositionCountingMoves("7k/P7/8/8/8/8/8/7K w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS, Move.NONE, 1, 0, 0, 1);
+		testPositionCountingMoves("7k/P7/8/8/8/8/8/7K w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS, null, 1, 0, 0, 1);
 
 		// Generates the underpromotion to rook
-		testPositionCountingMoves("7k/P7/8/8/8/8/8/7K w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS_CHECKS, Move.NONE, 2, 0, 0, 2);
+		testPositionCountingMoves("7k/P7/8/8/8/8/8/7K w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS_CHECKS, null, 2, 0, 0, 2);
 	}
 
 	@Test
 	public void testGenerateCapturesInQuiescence() {
-		testPositionCountingMoves("8/1kb2p2/4b1p1/8/2Q2NB1/8/8/K7 w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS, Move.NONE, 2, 2, 0, 0);
+		testPositionCountingMoves("8/1kb2p2/4b1p1/8/2Q2NB1/8/8/K7 w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS, null, 2, 2, 0, 0);
 	}
 
 	@Test
 	public void testGenerateOnlyGoodAnEqualCapturesInQuiescence() {
-		testPositionCountingMoves("2q4k/3n4/1p6/2b5/8/1N2B3/8/6QK w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS, Move.NONE, 2, 2, 0, 0);
+		testPositionCountingMoves("2q4k/3n4/1p6/2b5/8/1N2B3/8/6QK w - - 0 1", MoveIterator.GENERATE_CAPTURES_PROMOS, null, 2, 2, 0, 0);
 	}
 
 	@Test
 	public void testChess960Castling() {
-		testPositionCountingMoves("nqrkbbnr/pppppppp/8/8/8/8/PPPPPPPP/NQRKBBNR w KQkq - 0 1", MoveIterator.GENERATE_ALL, Move.NONE, 20, 0, 0, 0);
+		testPositionCountingMoves("nqrkbbnr/pppppppp/8/8/8/8/PPPPPPPP/NQRKBBNR w KQkq - 0 1", MoveIterator.GENERATE_ALL, null, 20, 0, 0, 0);
 	}
 
 	@Test
 	public void testChess960CastlingRookSameSquareGivesCheck() {
-		testPositionCountingMoves("8/8/8/8/8/8/8/3RK2k w Q - 0 1", MoveIterator.GENERATE_ALL, Move.NONE, 15, 0, 0, 4);
+		testPositionCountingMoves("8/8/8/8/8/8/8/3RK2k w Q - 0 1", MoveIterator.GENERATE_ALL, null, 15, 0, 0, 4);
 	}
 
 	@Test
 	public void testPawnPushDoesNotLeaveTheKingInCheck() {
-		testPositionCountingMoves("5rk1/6P1/2Q5/b5Pp/p6P/8/1rPK4/q3R1R1 w - - 3 6", MoveIterator.GENERATE_ALL, Move.NONE, 4, 0, 0, 0);
+		testPositionCountingMoves("5rk1/6P1/2Q5/b5Pp/p6P/8/1rPK4/q3R1R1 w - - 3 6", MoveIterator.GENERATE_ALL, null, 4, 0, 0, 0);
 	}
 
 	@Test
@@ -325,4 +323,18 @@ public class MoveIteratorTest {
 		}
 		assertEquals("Must not allow black long castling", false, longCastling);
 	}
+
+	@Test
+	public void testTtLastMoveSee() {
+		Board b = new Board();
+		b.setFen("rq2r1k1/5p2/p6p/4b1P1/1p2P2P/5Q2/PP4K1/5R1R w - - 0 2");
+		System.out.println(b.toString());
+		MoveIterator moveIterator = new MoveIterator(b, new AttacksInfo(), new SortInfo(), 0);
+		int ttMove = Move.getFromString(b, "Qc3", true);
+		moveIterator.genMoves(ttMove, MoveIterator.GENERATE_ALL);
+		int move = moveIterator.next();
+		assertEquals("Qc3", Move.toSan(b, move));
+		assertEquals(-900, moveIterator.getLastMoveSee());
+	}
+
 }
