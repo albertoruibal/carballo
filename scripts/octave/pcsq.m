0af2ef9b027392ce6d7ef4eebe763d19b3d0da14
albertoruibal <alberto@alonsoruibal.com>
2016-04-27 15:14:38 +0200

    Improve PCSQ tables plot

--- scripts/octave/pcsq.m
+++ scripts/octave/pcsq.m
@@ -111,15 +111,15 @@ function A = myDiagonal(D)
     A = [ (D1 + D2) fliplr(D1 + D2); flip(D1 + D2) flip(fliplr(D1 + D2))];
 endfunction
 
-function out = generatePCSQ(pieceValue,Column4, Rank, Diagonal, OpeningCorrection, plotOffset)
+function out = generatePCSQ(pieceValue,Column4, Rank, Diagonal, OpeningCorrection, plotOffset, pieceName)
     O = OpeningCorrection + pieceValue(1) + repmat([Column4(1, :) flip(Column4(1, :))], 8, 1) + repmat(Rank(1, :)', 1, 8) + myDiagonal(Diagonal(1, :));
     E = pieceValue(2) + repmat([Column4(2, :) flip(Column4(2, :))], 8, 1) + repmat(Rank(2, :)', 1, 8) + myDiagonal(Diagonal(2, :));
     subplot(6, 2, plotOffset);
     surf(O);
-    legend("Opening");
+    title(strcat(pieceName, " Opening"));
     subplot(6, 2, plotOffset + 1);
     surf(E);
-    legend("EndGame");
+    title(strcat(pieceName, " Endgame"));
         
     OE = [O(1,:) O(2,:) O(3, :) O(4,:) O(5,:) O(6,:) O(7,:) O(8,:) ; E(1,:) E(2,:) E(3,:) E(4,:) E(5,:) E(6,:) E(7,:) E(8,:)](:);
         
@@ -130,15 +130,20 @@ endfunction
 
 printf(strcat(
     "private static final int pawnPcsq[] =",
-    generatePCSQ([100, 100], PawnColumn, PawnRank, PawnDiagonal, PawnOpeningCorrection, 1),
+    generatePCSQ([100, 100], PawnColumn, PawnRank, PawnDiagonal, PawnOpeningCorrection, 1, "Pawn"),
     "private static final int knightPcsq[] =",
-    generatePCSQ([325, 325], KnightColumn, KnightRank, KnightDiagonal, 0, 3),
+    generatePCSQ([325, 325], KnightColumn, KnightRank, KnightDiagonal, 0, 3, "Knight"),
     "private static final int bishopPcsq[] =",
-    generatePCSQ([325, 325], BishopColumn, BishopRank, BishopDiagonal, 0, 5),
+    generatePCSQ([325, 325], BishopColumn, BishopRank, BishopDiagonal, 0, 5, "Bishop"),
     "private static final int rookPcsq[] =",
-    generatePCSQ([500, 500], RookColumn, RookRank, RookDiagonal, 0, 7),
+    generatePCSQ([500, 500], RookColumn, RookRank, RookDiagonal, 0, 7, "Rook"),
     "private static final int queenPcsq[] =",
-    generatePCSQ([975, 975], QueenColumn, QueenRank, QueenDiagonal, 0, 9),
+    generatePCSQ([975, 975], QueenColumn, QueenRank, QueenDiagonal, 0, 9, "Queen"),
     "private static final int kingPcsq[] =",
-    generatePCSQ([1000, 1000], KingColumn, KingRank, KingDiagonal, 0, 11)
+    generatePCSQ([1000, 1000], KingColumn, KingRank, KingDiagonal, 0, 11, "King")
     ));
+
+ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
+text(0.5, 1,'Carballo Piece-Square Tables','HorizontalAlignment', 'center', 'VerticalAlignment', 'top', 'FontSize', 25);
+
+#print -dpng "-S900,1500" "/tmp/carballo_pcsq.png"
