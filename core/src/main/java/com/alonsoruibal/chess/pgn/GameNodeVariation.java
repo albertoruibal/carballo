f3e8e7b155366224092e984b80695dc1713b5d64
albertoruibal <alberto@alonsoruibal.com>
2016-10-17 21:26:37 +0200

    Some utility methods for the GameNodeVariation

--- core/src/main/java/com/alonsoruibal/chess/pgn/GameNodeVariation.java
+++ core/src/main/java/com/alonsoruibal/chess/pgn/GameNodeVariation.java
@@ -6,6 +6,18 @@ public class GameNodeVariation extends GameNode {
 
 	public ArrayList<GameNode> variation = new ArrayList<>();
 
+	public void add(GameNode gameNode) {
+		variation.add(gameNode);
+	}
+
+	public GameNode get(int index) {
+		return variation.get(index);
+	}
+
+	public int size() {
+		return variation.size();
+	}
+
 	@Override
 	public String toString() {
 		return "GameNodeVariation{" +
