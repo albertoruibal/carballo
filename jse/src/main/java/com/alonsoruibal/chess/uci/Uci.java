6d5247216c34f801a6ea98141dda8c4ab122b7c6
Alberto Alonso Ruibal <alberto@alonsoruibal.com>
2026-08-03 10:21:53 +0200

    Bump version to 1.9 in Uci.java

--- jse/src/main/java/com/alonsoruibal/chess/uci/Uci.java
+++ jse/src/main/java/com/alonsoruibal/chess/uci/Uci.java
@@ -18,7 +18,7 @@ import java.io.InputStreamReader;
  * UCI Interface
  */
 public class Uci implements SearchObserver {
-	static final String NAME = "Carballo Chess Engine v1.8";
+	static final String NAME = "Carballo Chess Engine v1.9";
 	static final String AUTHOR = "Alberto Alonso Ruibal";
 
 	Config config;
